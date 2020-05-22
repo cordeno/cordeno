b7375f953ef83b8a160ce07b9ef30edb85728d07
ITS_N1GH7OWL <51875905+ITS-NIGHTOWL@users.noreply.github.com>
2020-05-22 02:33:51 -0600

    Initial commit

---
+++ deps.ts
@@ -0,0 +1,9 @@
+export {
+  connectWebSocket,
+  isWebSocketCloseEvent,
+  WebSocket,
+} from "https://deno.land/std/ws/mod.ts";
+
+export const LibraryData: object = {
+  url: "https://github.com/cordeno/cordeno",
+};
