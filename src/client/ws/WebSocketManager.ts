edd53c2f8a219ed6fc32ce1f681a7a8b5e4202bc
ITS_N1GH7OWL <51875905+ITS-NIGHTOWL@users.noreply.github.com>
2020-05-24 16:22:21 -0600

    Added heartbeat

--- src/client/ws/WebSocketManager.ts
+++ src/client/ws/WebSocketManager.ts
@@ -5,25 +5,34 @@ import { Client } from "../client.ts";
 
 export class WebSocketManager {
   private socket!: WebSocket;
+  private beatInterval!: number;
+  private beatRecieved: boolean = true;
   constructor(private client: Client) {
   }
 
   async connect() {
     this.socket = await connectWebSocket(Discord.Endpoint);
-
     for await (const msg of this.socket) {
       const payload: Payload = JSON.parse(msg.toString());
-      console.log(payload);
 
-      switch (payload.op) {
-        case OPCODE.Hello: {
-          this.identify();
+      if (typeof msg === "string") {
+        switch (payload.op) {
+          case OPCODE.Hello: {
+            this.identify();
+            this.heartbeatInterval(payload.d.heartbeat_interval);
+          }
+          case OPCODE.HeartbeatACK: {
+            console.log(payload);
+          }
         }
+      }
+      else {
+
       }
     }
   }
+
   async identify() {
-    console.log(this.client.options.token);
     this.socket.send(JSON.stringify({
       op: OPCODE.Identify,
       d: {
@@ -35,5 +44,26 @@ export class WebSocketManager {
         },
       },
     }));
+  }
+
+  async heartbeatInterval(rate: number) {
+    if (this.beatRecieved) {
+      this.beatInterval = setInterval(() => {
+        this.heartbeat()
+      }, rate);
+      this.beatRecieved = false;
+    } else {
+      this.panic();
+    }
+  }
+
+  async heartbeat() {
+    this.socket.send(JSON.stringify({
+      op: OPCODE.Heartbeat,
+      d: null,
+    }));
+  }
+  async panic() {
+    //Reconnection code here
   }
 }
--- tests/bot.ts
+++ tests/bot.ts
@@ -1,7 +1,10 @@
 import { Client } from "../mod.ts";
+import * as dotenv from 'https://deno.land/x/denoenv/mod.ts'
+const env = dotenv.config()
 
+console.log(env.TOKEN)
 const client = new Client({
-  token: "TOKEN",
+  token: env.TOKEN,
 });
 
 console.log(`Running cordeno v${client.version}`);
