1b6a484e461fd6694d7857741b3562b1109876da
ITS_N1GH7OWL <51875905+ITS-NIGHTOWL@users.noreply.github.com>
2020-05-25 00:42:07 -0600

    Fixed import name

--- src/client/ws/WebSocketManager.ts
+++ src/client/ws/WebSocketManager.ts
@@ -1,7 +1,7 @@
 import { connectWebSocket, WebSocket, EventEmitter } from "../../../deps.ts";
 import { Discord, Payload, OPCODE } from "../constant/discord.ts";
 import { Cordeno } from "../constant/cordeno.ts";
-import { Client } from "../client.ts";
+import { Client } from "../Client.ts";
 import { AsyncEventQueue } from "../Queue.ts";
 
 export class WebSocketManager extends EventEmitter {
