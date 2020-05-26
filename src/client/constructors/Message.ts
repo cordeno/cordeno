da758e757eb2241db047a5a5e1b4c8de25fbb57e
ITS_N1GH7OWL <51875905+ITS-NIGHTOWL@users.noreply.github.com>
2020-05-26 02:07:48 -0600

    Removed ranomd `?` marks

--- src/client/constructors/Message.ts
+++ src/client/constructors/Message.ts
@@ -5,9 +5,9 @@ export class Message implements Interfaces.Message {
   public event!: string;
   id!: string;
   channel_id!: string;
-  guild_id?: string;
+  guild_id!: string;
   author!: Interfaces.User;
-  member?: Interfaces.GuildMember;
+  member!: Interfaces.GuildMember;
   content!: string;
   timestamp!: string;
   edited_timestamp!: string;
@@ -15,18 +15,18 @@ export class Message implements Interfaces.Message {
   mention_everyone!: boolean;
   mentions!: Array<Interfaces.User & Interfaces.GuildMember>;
   mention_roles!: Array<Interfaces.Role>;
-  mention_channels?: Array<Interfaces.ChannelMention>;
+  mention_channels!: Array<Interfaces.ChannelMention>;
   attachments!: Array<Interfaces.Attachment>;
   embeds!: Array<Interfaces.Embed>;
-  reactions?: Array<Interfaces.Reaction>;
-  nonce?: number | string;
+  reactions!: Array<Interfaces.Reaction>;
+  nonce!: number | string;
   pinned!: boolean;
-  webhook_id?: string;
+  webhook_id!: string;
   type!: number;
-  activity?: Interfaces.Activity;
-  application?: Interfaces.Application;
-  message_reference?: Interfaces.MessageReference;
-  flags?: number;
+  activity!: Interfaces.Activity;
+  application!: Interfaces.Application;
+  message_reference!: Interfaces.MessageReference;
+  flags!: number;
 
   constructor(public client: Client, public payload: any) {
   }
--- tests/bot.ts
+++ tests/bot.ts
@@ -11,6 +11,7 @@ console.log(`Running cordeno v${client.version}`);
 for await (const ctx of client) {
   if (ctx.event === "MESSAGE_CREATE") {
     const msg: Message = ctx;
+    console.log(msg.member.roles)
     if (msg.author.id !== client.user.id) {
       if (msg.content === "!ping") {
         msg.reply("Pong!");
