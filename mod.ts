3645fff4f0c956829240c19191a3209de4db17ad
ITS_N1GH7OWL <51875905+ITS-NIGHTOWL@users.noreply.github.com>
2020-06-05 04:04:14 -0600

    Export all constructors

--- mod.ts
+++ mod.ts
@@ -1,9 +1,2 @@
 export { Client } from "./src/client/Client.ts";
-export {
-  Message,
-  Ready,
-  Ratelimit,
-  Heartbeat,
-  Resumed,
-  InvalidSession,
-} from "./src/client/constructors/constructor_export.ts";
+export * from "./src/client/constructors/constructor_export.ts";
