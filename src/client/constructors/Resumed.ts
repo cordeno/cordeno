ae174cf54418993f0cd522adbdddc07000e1fa60
ITS_N1GH7OWL <51875905+ITS-NIGHTOWL@users.noreply.github.com>
2020-06-05 18:12:34 -0600

    Delete Resumed.ts

--- src/client/constructors/Resumed.ts
+++
@@ -1,9 +0,0 @@
-import { Client } from "../Client.ts";
-
-export class RESUMED {
-  public resumeTime!: Date;
-
-  constructor(private client: Client, private payload: any) {
-    this.resumeTime = new Date();
-  }
-}
