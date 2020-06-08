68ac8cef1282acd438f7c8a3e3f74acd0fa6084c
ITS_N1GH7OWL <51875905+ITS-NIGHTOWL@users.noreply.github.com>
2020-06-07 22:28:14 -0600

    Inplemented first structure

--- src/client/constructors/MESSAGE_CREATE.ts
+++ src/client/constructors/MESSAGE_CREATE.ts
@@ -1,4 +1,7 @@
+// https://discord.com/developers/docs/resources/channel#message-object
+
 import { Client } from "../Client.ts";
+import { UserStruct } from './struct/User.ts'
 import * as Interfaces from "../interfaces/interface_export.ts";
 
 export interface MessageOptions {
@@ -7,7 +10,7 @@ export interface MessageOptions {
 }
 
 export class MESSAGE_CREATE {
-  public author!: Interfaces.User;
+  public author!: UserStruct;
   public createdAt!: Date;
   public editedAt!: Date | null;
   public content!: string;
@@ -15,7 +18,7 @@ export class MESSAGE_CREATE {
 
   constructor(private client: Client, private payload: any) {
     const data: Interfaces.Message = this.payload.d;
-    this.author = data.author;
+    this.author = new UserStruct(data.author);
     this.createdAt = new Date(data.timestamp);
     this.editedAt = (data.edited_timestamp)
       ? new Date(data.edited_timestamp)
